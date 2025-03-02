542a87336dceb0237c703fc6c24a5c02438515e0
davismasondaniel <108919916+davismasondaniel@users.noreply.github.com>
2025-03-02 02:52:24 +0000

    jdmrims
    jdmrims

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'data/*.meta',
+   }
+
+data_file 'CARCOLS_FILE' 'data/carcols.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/jdmrimscontentunlocks.meta'
